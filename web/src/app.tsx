a2a0b13e00cfdea43c8821dca410d5995c1db319
Bruno Becoski <brunobecoski@gmail.com>
2024-09-14 17:10:24 -0300

    Cria a função de buscar as metas

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,17 +1,23 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { getSummary } from './http/get-summary'
 import { Dialog } from './components/ui/dialog'
 import { EmptyGoals } from './components/empty-goals'
-import { Summary } from './components/summary';
+import { Summary } from './components/summary'
 import { CreateGoal } from './components/create-goal'
 
 export function App() {
+  const { data } = useQuery({
+		queryKey: ['get-summary'],
+		queryFn: getSummary
+	})
+
 	return (
 		<Dialog>
-			{/* <EmptyGoals /> */}
-
-			<Summary />
+			{data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
 
 			<CreateGoal />
 		</Dialog>
-	);
+	)
 }
   
